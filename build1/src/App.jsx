470e2776869c23b23755041f1631c97625d5c538
Saurav <sauravjyotikalita56@gmail.com>
2025-04-19 13:43:07 +0530

    Removed Unnecessary files

--- build1/src/App.jsx
+++ build1/src/App.jsx
@@ -4,7 +4,6 @@ import CareerReadinessSection from './components/CareerReadinessSection';
 import Homepage from './components/HomePage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProfileSection from './components/ProfileSection';
-import ResourceSection from './components/ResourceSection';
 import Footer from './components/Footer'; // Uncomment or remove based on usage
 import AboutUs from './components/AboutUs';
 import CommunitySection from './components/CommunitySection';
@@ -22,7 +21,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/ProfileSection" element={<ProfileSection />} />
-          <Route path="/ResourceSection" element={<ResourceSection />} />
           <Route path="/CareerReadinessSection" element={<CareerReadinessSection />} />
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/CommunitySection" element={<CommunitySection />} />
